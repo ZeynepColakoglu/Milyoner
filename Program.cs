0c52479ce7fb0a4e83a08ca426d8ff8972f7c1ec
zeynep.colakoglu <zeynep.colakoglu@305-03>
2023-04-01 11:28:54 +0300

    feat:joker

--- Program.cs
+++ Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Kim Milyoner Olmak İster - V1
+            Console.WriteLine("Kim Milyoner Olmak İster\n");
+            Console.Write("İsim Giriniz: ");
+            string isim = Console.ReadLine();
+            Console.Write("Soyisim Giriniz: ");
+            string soyisim = Console.ReadLine();
+            Console.Write("\n1.Telefon Jokerinizi Giriniz: ");
+            string telefonJoker1 = Console.ReadLine();
+            Console.Write("2.Telefon Jokerinizi Giriniz: ");
+            string telefonJoker2 = Console.ReadLine();
+            Console.Write("3.Telefon Jokerinizi Giriniz: ");
+            string telefonJoker3 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Hoşgeldiniz " + isim + " " + soyisim);
+
+            Console.Write("Kuralları Biliyor Musunuz? : ");
+            char kuralCevap = char.Parse(Console.ReadLine().ToUpper());
+
+            if (kuralCevap != 'E')
+            {
+                // TODO: Kurallar eklenecek.
+                Console.WriteLine("Kurallar: asdfasdaads");
+            }
+
+            Console.Write("Hazır Mısınız? : ");
+            char hazirCevap = char.Parse(Console.ReadLine().ToUpper());
+            if (hazirCevap != 'E')
+            {
+                Console.WriteLine("Hazır olmananız bekleniyor.");
+                Console.WriteLine("Hazır olduğunuzda Enter'a Basın");
+                Console.ReadLine();
+            }
+            bool seyirciHak = true, yuzdeHak = true, telefonHak = true;
+            int kasa = 0;
+
+            int soruNo = 1, odul, baraj;
+            int seyirciYuzdeA, seyirciYuzdeB, seyirciYuzdeC, seyirciYuzdeD;
+            bool gizleA, gizleB, gizleC, gizleD;
+            string soru, a, b, c, d;
+            char cevap;
+
+            #region Soru Bilgileri
+            soruNo = 1;
+            soru = "Türkiyenin Başkenti Neresidir?";
+            a = "Ankara";
+            b = "Bursa";
+            c = "Van";
+            d = "Denizli";
+            cevap = 'A';
+            odul = 1000;
+            baraj = 0;// TODO: SoruNo ile baraj ilişkisi.
+
         }
     }
 }
